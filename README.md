202176e639035f22250e7d4eb72756458b0d3719
chococuki <kkang13277@naver.com>
2023-04-30 20:07:53 +0900

    [Add] get - create user account

--- README.md
+++ README.md
@@ -15,10 +15,69 @@
 * 각 요구사항을 구현하는 것이 중요한 것이 아니라 구현 과정을 통해 학습한 내용을 인식하는 것이 배움에 중요하다. 
 
 ### 요구사항 1 - http://localhost:8080/index.html로 접속시 응답
-* 
+* InputStreamReader를 통해 소켓의 InputStream을 읽음
+* InputStreamReader로 읽은 것을 BufferedReader 클래스 객체 생성 시 생성자에 넣음
+* 첫번째 라인에 http method, 요청 url이 담겨 있음
+	* ex) GET/index.html HTTP/1.1
+* line 변수를 split 함수를 사용해서 공백을 기준으로 나눠 String 배열인 tokens 변수에 할당.
+* tokens 변수의 [1] 번째 인덱스에 있는 요청 URL만 String 자료형 url 변수에 할당.
+* Files와 File 클래스를 사용해서 ./webapp 경로에 있는 파일들을 byte[] 형식으로 전부 읽음.
+* 읽어온 bytes 값을 DataOutputStream 클래스를 사용해 write.
+
+```java
+try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+    	InputStreamReader inputStreamReader = new InputStreamReader(in);
+    	BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+    	
+    	String line = bufferedReader.readLine();
+    	
+    	if(line == null) {
+    		return;
+    	}
+    	
+    	String[] tokens = line.split(" ");
+    	
+    	String url = tokens[1];
+    	
+    	log.debug("connect URL : {}", url);
+    	
+    	byte[] bytes = Files.readAllBytes(new File("./webapp" + url).toPath());
+    	
+    	DataOutputStream dos = new DataOutputStream(out);
+        response200Header(dos, bytes.length);
+        responseBody(dos, bytes);
+    } catch (IOException e) {
+        log.error(e.getMessage());
+}
+```
+
+	-> css 파일을 적용하지 못하고 있음
 
 ### 요구사항 2 - get 방식으로 회원가입
-* 
+* 회원가입 버튼을 누르면 아래와 같은 형태로 서버에 전달됨
+	* /user/create?userId=testid&password=1234&name=test&email=test@gmail.com
+* uri에 user/create 가 포함되어있는지 확인후 유저 정보를 DataBase에 저장
+
+	```
+	if(uri.contains("/create")) {
+    		String queryString = uri.split("\\?")[1];
+    		
+    		Map<String, String> userInfo = HttpRequestUtils.parseQueryString(queryString);
+    		
+    		String userId = userInfo.getOrDefault("userId", "");
+    		String password = userInfo.getOrDefault("password", "");
+    		String name = userInfo.getOrDefault("name", "");
+    		String email = userInfo.getOrDefault("email", "");
+    		
+    		User user = new User(userId, password, name, email);
+    		
+    		DataBase.addUser(user);
+    		log.debug("add user : {}", user.toString());
+    		
+    		return true;
+    	}
+    	```
+ * 
 
 ### 요구사항 3 - post 방식으로 회원가입
 * 
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,25 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.http.HttpRequest;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import db.DataBase;
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,14 +35,61 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+        	InputStreamReader inputStreamReader = new InputStreamReader(in);
+        	BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        	
+        	String line = bufferedReader.readLine();
+        	
+        	if(line == null) {
+        		return;
+        	}
+        	
+        	String[] tokens = line.split(" ");
+        	
+        	String uri = tokens[1];
+        	
+        	if(checkLink(uri)) {
+        		return;
+        	}
+        	
+        	log.debug("connect URL : {}", uri);
+        	
+        	Path path = Paths.get("./webapp", uri);
+        	if(!Files.exists(path)) {
+        		log.debug("path not exist : {}", path);
+        		return;
+        	}
+        	
+        	byte[] bytes = Files.readAllBytes(new File("./webapp" + uri).toPath());
+        	
+        	DataOutputStream dos = new DataOutputStream(out);
+            response200Header(dos, bytes.length);
+            responseBody(dos, bytes);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private boolean checkLink(String uri) {
+    	if(uri.contains("/create")) {
+    		String queryString = uri.split("\\?")[1];
+    		
+    		Map<String, String> userInfo = HttpRequestUtils.parseQueryString(queryString);
+    		
+    		String userId = userInfo.getOrDefault("userId", "");
+    		String password = userInfo.getOrDefault("password", "");
+    		String name = userInfo.getOrDefault("name", "");
+    		String email = userInfo.getOrDefault("email", "");
+    		
+    		User user = new User(userId, password, name, email);
+    		
+    		DataBase.addUser(user);
+    		log.debug("add user : {}", user.toString());
+    		
+    		return true;
+    	}
+    	
+    	return false;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
